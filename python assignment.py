06d73beab6618a7368c53ab26647a2292f45be8d
himanshu vashistha <113257378+coderstar1234@users.noreply.github.com>
2023-04-01 07:38:58 +0530

    Add files via upload

---
+++ python assignment.py
@@ -0,0 +1,7 @@
+//To use this function, simply pass in a list of numbers as an argument:
+
+python
+Copy code
+my_list = [1, 2, 3, 4, 5]
+result = sum_squares(my_list)
+print(result)  # Output: 55   return sum
